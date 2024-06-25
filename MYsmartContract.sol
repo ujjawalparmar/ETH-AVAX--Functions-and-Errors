ea76af17e0033413887472726c66031314125080
ujjawalparmar <ujjawalparmar370@gmail.com>
2024-06-25 13:25:16 +0530

    Update MYsmartContract.sol

--- MYsmartContract.sol
+++ MYsmartContract.sol
@@ -2,18 +2,20 @@
 pragma solidity ^0.8.1;
 
 contract MYsmartContract {
-    uint public mass = 10;
-    uint public volume = 5; // Assuming a volume value for the correction
+    uint public mass = 100;
+    uint public volume = 50;
     address public owner;
 
     constructor() {
         owner = msg.sender;
+
     }
+
     function Weight(uint _density) public view returns (uint) {
-    require(owner == msg.sender, "You are not the owner");
+    require(owner == msg.sender, "You are not the owner of this");
     assert(volume > 0);
         if ((_density * volume) == 0) {
-            revert("Weight of the object cannot be zero");
+            revert("Weight of the object can't be zero");
         }
 
         return _density * volume;
